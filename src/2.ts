5f44a41e147c8aac915a0c02ba88ea2367e4db6e
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-03-02 15:30:08 +0200

    done-1

--- src/2.ts
+++ src/2.ts
@@ -16,14 +16,18 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  // name: string;
+  // department: string;
+  // salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
+    // this.name = name;
+    // this.department = department;
+    // this.salary = salary;
   }
 
   getEmployeeDetails() {
@@ -32,8 +36,10 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
-
 
 export {};
